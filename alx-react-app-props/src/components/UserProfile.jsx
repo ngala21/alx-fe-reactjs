0fb22c5d031f4b7c7195392b2a33658465e0c09e
Ngala21 <ngalamike21@gmail.com>
2024-08-18 21:45:15 +0300

    refactoring..

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserContext from './UserContext';
+
+function UserProfile () {
+    const userData = useContext(UserContext);
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
+      </div>
+    );
+  };
+
+  export default UserProfile;
